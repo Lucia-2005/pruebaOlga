81867f5aa23f4901b075a6ee66fa7ec17260f3b9
LUCIA <castaneralucia@gmail.com>
2025-05-21 13:39:57 +0200

    miau v3

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("ya no soy Olga Garcia Quintana");
         System.out.println("miau miau miau");
-        System.out.println("ahra soy Haru!");
+        System.out.println("ahora soy Haru!");
+
+        int suma=2+4;
     }
 }
